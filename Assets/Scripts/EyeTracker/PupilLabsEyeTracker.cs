d3975ec322fc041f59bb08544d921204fbae5e4f
JFrit <jf3925@rit.edu>
2021-05-03 17:47:23 -0400

    Start PupilLabs recording automatically on eye tracker connect

--- Assets/Scripts/EyeTracker/PupilLabsEyeTracker.cs
+++ Assets/Scripts/EyeTracker/PupilLabsEyeTracker.cs
@@ -5,8 +5,11 @@ namespace EyeTracker
 {
     public class PupilLabsEyeTracker : MonoBehaviour, IEyeTracker
     {
+        [SerializeField] private RequestController requestController;
         [SerializeField] private GazeController gazeController;
+        [SerializeField] private RecordingController recordingController;
         [SerializeField] [Range(0,1)] private float confidenceThreshold;
+        
 
         private Vector3 _localGazeDirection;
 
@@ -14,7 +17,15 @@ namespace EyeTracker
         {
             
             _localGazeDirection = Vector3.forward;
+            requestController.OnConnected += recordingController.StartRecording;
             gazeController.OnReceive3dGaze += ReceiveGaze;
+        }
+        
+        public void OnDisable()
+        {
+            
+            requestController.OnConnected -= recordingController.StartRecording;
+            gazeController.OnReceive3dGaze -= ReceiveGaze;
         }
         
         private void ReceiveGaze(GazeData gazeData)
